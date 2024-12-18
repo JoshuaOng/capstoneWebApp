3e29a7bb4f63584bbb11ab157c5331dd37785378
JoshuaOng <joshua.ong2018@gmail.com>
2024-12-18 12:13:38 +0800

    test

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const paymentRoutes = require('./routes/payment');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
+// Routes
+app.use('/api/payment', paymentRoutes);
+
+// Test endpoint
+app.get('/api', (req, res) => {
+  res.send('Backend is running!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Backend running at http://localhost:${PORT}`);
+});
+
+
+
