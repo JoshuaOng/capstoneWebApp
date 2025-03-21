3ad0d5b912c68c2a4c0db8bf13a8a0b993d5e01f
JoshuaOng <joshua.ong2018@gmail.com>
2025-03-22 00:26:35 +0800

    update stripe keys

--- routes/payment.js
+++ routes/payment.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const stripe = require('stripe')('sk_test_51QJnma08yS02cjemGFJ27o2K6rNlk2OcuAz17xLA3nlwB5N3d0HL7McwLrFx911KPjD5vvc5F3Hm5X7kAlSWvNcY008DFvVyPR');
-// const stripe = require('stripe')('sk_test_51QztLCQ1fayfJvU18pcD9ev1EcFYJuBipLHynCzuPUzYG1weEthyL3X5HLhBkR4VJCHpIdhaYQ9XeFBEJ1HwMD0900t0Bbi6ZM');//prof acc
+// const stripe = require('stripe')('sk_test_51QJnma08yS02cjemGFJ27o2K6rNlk2OcuAz17xLA3nlwB5N3d0HL7McwLrFx911KPjD5vvc5F3Hm5X7kAlSWvNcY008DFvVyPR');
+const stripe = require('stripe')('sk_test_51QztLCQ1fayfJvU18pcD9ev1EcFYJuBipLHynCzuPUzYG1weEthyL3X5HLhBkR4VJCHpIdhaYQ9XeFBEJ1HwMD0900t0Bbi6ZM');//prof acc
 
 
 // Create a PaymentIntent
